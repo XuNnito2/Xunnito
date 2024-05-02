1dfa8bb8aa1cac769d98491083b324ed10078ff7
XuNnito2 <167809598+XuNnito2@users.noreply.github.com>
2024-05-01 18:12:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-$("#showmenu").click(function(e){
-			$("#menu").toggleClass("show");
-		});
-		$("#menu a").click(function(event){
-			if($(this).next('ul').length){
+("#showmenu").click(function(e) {
+    $("#menu").toggleClass("show");
+    if ($("#menu").hasClass("show")) {
+        setTimeout(function() {
+            $("#menu").removeClass("show");
+        }, 3000);
+    }
+});
+
+$("#menu a").click(function(event) {
+    if ($(this).next('ul').length) {
         event.preventDefault();
-				$(this).next().toggle('fast');
-				$(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
-			}
-		});
+        $(this).next().toggle('fast');
+        $(this).children('i:last-child').toggleClass('fa-caret-down fa-caret-left');
+    }
+});
