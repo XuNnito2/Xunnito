a502bbad80cfda1956de9adfc2fdb15329e3fb56
XuNnito2 <167809598+XuNnito2@users.noreply.github.com>
2024-10-26 15:21:46 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -90,21 +90,3 @@ document.addEventListener('DOMContentLoaded', function () {
 function cerrarAnuncio() {
     document.getElementById('anuncio').style.display = 'none';
 }
-
-
-// ==========================================
-// $(window).on('load',function () {
-//     var current = location.pathname;
-//     console.log(current);
-//     $('#navbarSupportedContent ul li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.parent().addClass('active');
-//             $this.parents('.menu-submenu').addClass('show-dropdown');
-//             $this.parents('.menu-submenu').parent().addClass('active');
-//         }else{
-//             $this.parent().removeClass('active');
-//         }
-//     })
-// });
